193b7903697017fac395691c40dd972ffc374fa4
Jordan Knight <jakkaj@gmail.com>
2016-07-29 15:33:25 -0700

    Added discovery and messaging to allow the local app to talk to itself and send things

---
+++ src/PhoneDump.Services/PhoneDump.Services/Messages/DumpReceivedMessage.cs
@@ -0,0 +1,20 @@
+﻿using PhoneDump.Entity.Dumps;
+using XamlingCore.Portable.Messages.XamlingMessenger;
+
+namespace PhoneDump.Services.Messages
+{
+    /// <summary>
+    /// Used when dumps are received to marshal from API to Services. Not for VM's
+    /// </summary>
+    public class DumpReceivedMessage : XMessage
+    {
+        private readonly DumpWireEntity _entity;
+
+        public DumpReceivedMessage(DumpWireEntity entity)
+        {
+            _entity = entity;
+        }
+
+        public DumpWireEntity Entity => _entity;
+    }
+}
--- src/PhoneDumpClient/PhoneDumpClient/PhoneDumpClient/View/HomeView.model.cs
+++ src/PhoneDumpClient/PhoneDumpClient/PhoneDumpClient/View/HomeView.model.cs
@@ -7,8 +7,10 @@ using XamlingCore.Portable.View.ViewModel;
 using PhoneDumpClient.Services;
 using Xamarin.Forms;
 using System.Diagnostics;
+using System.Windows.Input;
 using PhoneDump.Contract.Services;
 using PhoneDump.Entity.Dumps;
+using XamlingCore.Portable.View;
 
 namespace PhoneDumpClient.View
 {
@@ -16,23 +18,40 @@ namespace PhoneDumpClient.View
     {
         private readonly ITokenService _tokenService;
         private readonly ITokenTestService _testService;
+        private readonly IFilePickerService _filePickerService;
+        private readonly ISendDumpService _sendDumpService;
         public string MainText { get; set; }
 
+        public ICommand TestButtonCommand { get; set; }
+
         public HomeViewModel(ITokenService tokenService, ITokenTestService testService,
-            IFilePickerService filePickerService)
+            IFilePickerService filePickerService,
+            ISendDumpService sendDumpService)
         {
+
+            TestButtonCommand = new XCommand(_onTestButton);
+
             _tokenService = tokenService;
             _testService = testService;
+            _filePickerService = filePickerService;
+            _sendDumpService = sendDumpService;
             MainText = "Jordan";
+            
+        }
+
+        async void _onTestButton()
+        {
             Device.BeginInvokeOnMainThread(async () =>
             {
-                var str = await filePickerService.GetFileStringAsync();
+                var str = await _filePickerService.GetFileStringAsync();
                 var entity = new DumpWireEntity
                 {
                     Id = Guid.NewGuid(),
                     EncodedData = str,
-                    MediaType = "something"
+                    MediaType = "something",
+                    RawData = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
                 };
+                await _sendDumpService.SendDump(entity);
             });
         }
 
